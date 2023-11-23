d2bf082205689e788cfcb93c35f5c2d93246b7bc
yiqqing <pqab0984278453@gmail.com>
2023-11-24 01:08:34 +0800

    train twice

--- test.py
+++ test.py
@@ -1,21 +1,3 @@
-import os
+import sys
 
-source = os.walk("dataset_copy")
-
-for res in source:
-    print(res)
-
-print("==========")
-source = os.walk("dataset_copy") # 因為迭代器在遍歷一次後就會指向末尾，再次遍歷時就不會返回任何內容。
-for folder, subfolders, files in source:
-    print(folder)
-    for subfolder in subfolders:
-        print(f'{folder} 的子資料夾 {subfolder}')
-    for file in files:
-        print(f'{folder} 的檔案有 {file}.')
-
-print("==========")
-ids = next(os.walk("dataset_copy/train/Images"))
-print(ids)
-print("No. of images = ", len(ids[2]))
-
+print(sys.version)
